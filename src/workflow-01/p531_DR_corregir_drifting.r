c18a33607679ac66e4584f2d16d7989ee985f753
plcablinski <plcablinski@gmail.com>
2024-04-30 19:12:06 +0000

    Elimina engdig

--- src/workflow-01/p531_DR_corregir_drifting.r
+++ src/workflow-01/p531_DR_corregir_drifting.r
@@ -152,7 +152,7 @@ AgregarVariables_IntraMes <- function(dataset) {
   dataset[, interaccant := rowSums(cbind(ccallcenter_transacciones, chomebanking_transacciones, ccajas_transacciones, ccajas_consultas, 
                                          ccajas_depositos, ccajas_extracciones, ccajas_otras, catm_trx, catm_trx_other, ctrx_quarter, 
                                          cmobile_app_trx), na.rm = TRUE)]
-  dataset[, engdig := rowSums(cbind(tcallcenter, thomebanking, tmobile_app), na.rm = TRUE)]
+  
   dataset[, transftotpesos := rowSums(cbind(mtransferencias_recibidas, mtransferencias_emitidas), na.rm = TRUE)]
   dataset[, transftotcant := rowSums(cbind(ctransferencias_recibidas, ctransferencias_emitidas), na.rm = TRUE)]
   dataset[, cheqtotpesos := rowSums(cbind(mcheques_depositados, mcheques_emitidos, mcheques_depositados_rechazados, mcheques_emitidos_rechazados), na.rm = TRUE)]
