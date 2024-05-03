1574965cc1bb44a6649a63558b1e6365c31d63f3
plcablinski <plcablinski@mail.austral.edu.ar>
2024-05-03 00:48:46 -0300

    Fuerza volumen ponderado como entero

--- src/workflow-01/wgpcimport531_DR_corregir_drifting.r
+++ src/workflow-01/wgpcimport531_DR_corregir_drifting.r
@@ -189,7 +189,7 @@ AgregarVariables_IntraMes <- function(dataset) {
   dataset[, solidez := rowSums(cbind(saldoyvolumen, saldoytotcuentas), na.rm = TRUE)]
   
   dataset[, saldofactores := rowSums(cbind(saldoyantig,saldoyedad, saldoydescubierto,saldoytotcuentas), na.rm = TRUE)]
-  dataset[, volumenpond:= as.integer(rowSums(cbind(totalpayrollpesos, totalgastospesos), na.rm = TRUE)*solidez)]
+  dataset[, volumenponderado:= as.integer(rowSums(cbind(totalpayrollpesos, totalgastospesos), na.rm = TRUE)*solidez)]
   
   dataset[, vm_mtot_transacciones_deb_cred := ctarjeta_debito_transacciones + ctarjeta_visa_transacciones + ctarjeta_master_transacciones ]
   cat( "\n","Fin variables combinadas agregadas")
