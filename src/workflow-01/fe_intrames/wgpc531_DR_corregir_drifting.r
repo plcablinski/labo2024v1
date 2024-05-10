9cd39e895bac76b3522ad2aaac04773d379834ab
plcablinski <plcablinski@mail.austral.edu.ar>
2024-05-10 00:43:31 -0300

    Pruebo iteración 2 sin las variables de la cátedra

--- src/workflow-01/fe_intrames/wgpc531_DR_corregir_drifting.r
+++ src/workflow-01/fe_intrames/wgpc531_DR_corregir_drifting.r
@@ -32,6 +32,12 @@ options(error = function() {
 # Parametros del script
 PARAM <- read_yaml( "parametros.yml" )
 
+# Si PARAM$variables_catedra no esta definido, lo defino y seteo en TRUE
+if( !exists("PARAM$variables_catedra") ) {
+  PARAM$variables_catedra <- TRUE
+}
+
+
 # Si PARAM$variables_iter1 no esta definido, lo defino y seteo en TRUE
 if( !exists("PARAM$variables_iter1") ) {
   PARAM$variables_iter1 <- TRUE
@@ -57,90 +63,93 @@ AgregarVariables_IntraMes <- function(dataset) {
   gc()
   # INICIO de la seccion donde se deben hacer cambios con variables nuevas
 
-  # creo un ctr_quarter que tenga en cuenta cuando
-  # los clientes hace 3 menos meses que estan
-  cat( "\n","Inicia variables intrames cátedra")
-  dataset[, ctrx_quarter_normalizado := ctrx_quarter]
-  dataset[cliente_antiguedad == 1, ctrx_quarter_normalizado := ctrx_quarter * 5]
-  dataset[cliente_antiguedad == 2, ctrx_quarter_normalizado := ctrx_quarter * 2]
-  dataset[cliente_antiguedad == 3, ctrx_quarter_normalizado := ctrx_quarter * 1.2]
-
-  # variable extraida de una tesis de maestria de Irlanda
-  dataset[, mpayroll_sobre_edad := mpayroll / cliente_edad]
-
-  # se crean los nuevos campos para MasterCard  y Visa,
-  #  teniendo en cuenta los NA's
-  # varias formas de combinar Visa_status y Master_status
-  dataset[, vm_status01 := pmax(Master_status, Visa_status, na.rm = TRUE)]
-  dataset[, vm_status02 := Master_status + Visa_status]
-
-  dataset[, vm_status03 := pmax(
-    ifelse(is.na(Master_status), 10, Master_status),
-    ifelse(is.na(Visa_status), 10, Visa_status)
-  )]
-
-  dataset[, vm_status04 := ifelse(is.na(Master_status), 10, Master_status)
-    + ifelse(is.na(Visa_status), 10, Visa_status)]
-
-  dataset[, vm_status05 := ifelse(is.na(Master_status), 10, Master_status)
-    + 100 * ifelse(is.na(Visa_status), 10, Visa_status)]
-
-  dataset[, vm_status06 := ifelse(is.na(Visa_status),
-    ifelse(is.na(Master_status), 10, Master_status),
-    Visa_status
-  )]
-
-  dataset[, mv_status07 := ifelse(is.na(Master_status),
-    ifelse(is.na(Visa_status), 10, Visa_status),
-    Master_status
-  )]
-
-
-  # combino MasterCard y Visa
-  dataset[, vm_mfinanciacion_limite := rowSums(cbind(Master_mfinanciacion_limite, Visa_mfinanciacion_limite), na.rm = TRUE)]
-
-  dataset[, vm_Fvencimiento := pmin(Master_Fvencimiento, Visa_Fvencimiento, na.rm = TRUE)]
-  dataset[, vm_Finiciomora := pmin(Master_Finiciomora, Visa_Finiciomora, na.rm = TRUE)]
-  dataset[, vm_msaldototal := rowSums(cbind(Master_msaldototal, Visa_msaldototal), na.rm = TRUE)]
-  dataset[, vm_msaldopesos := rowSums(cbind(Master_msaldopesos, Visa_msaldopesos), na.rm = TRUE)]
-  dataset[, vm_msaldodolares := rowSums(cbind(Master_msaldodolares, Visa_msaldodolares), na.rm = TRUE)]
-  dataset[, vm_mconsumospesos := rowSums(cbind(Master_mconsumospesos, Visa_mconsumospesos), na.rm = TRUE)]
-  dataset[, vm_mconsumosdolares := rowSums(cbind(Master_mconsumosdolares, Visa_mconsumosdolares), na.rm = TRUE)]
-  dataset[, vm_mlimitecompra := rowSums(cbind(Master_mlimitecompra, Visa_mlimitecompra), na.rm = TRUE)]
-  dataset[, vm_madelantopesos := rowSums(cbind(Master_madelantopesos, Visa_madelantopesos), na.rm = TRUE)]
-  dataset[, vm_madelantodolares := rowSums(cbind(Master_madelantodolares, Visa_madelantodolares), na.rm = TRUE)]
-  dataset[, vm_fultimo_cierre := pmax(Master_fultimo_cierre, Visa_fultimo_cierre, na.rm = TRUE)]
-  dataset[, vm_mpagado := rowSums(cbind(Master_mpagado, Visa_mpagado), na.rm = TRUE)]
-  dataset[, vm_mpagospesos := rowSums(cbind(Master_mpagospesos, Visa_mpagospesos), na.rm = TRUE)]
-  dataset[, vm_mpagosdolares := rowSums(cbind(Master_mpagosdolares, Visa_mpagosdolares), na.rm = TRUE)]
-  dataset[, vm_fechaalta := pmax(Master_fechaalta, Visa_fechaalta, na.rm = TRUE)]
-  dataset[, vm_mconsumototal := rowSums(cbind(Master_mconsumototal, Visa_mconsumototal), na.rm = TRUE)]
-  dataset[, vm_cconsumos := rowSums(cbind(Master_cconsumos, Visa_cconsumos), na.rm = TRUE)]
-  dataset[, vm_cadelantosefectivo := rowSums(cbind(Master_cadelantosefectivo, Visa_cadelantosefectivo), na.rm = TRUE)]
-  dataset[, vm_mpagominimo := rowSums(cbind(Master_mpagominimo, Visa_mpagominimo), na.rm = TRUE)]
+  if (PARAM$variables_catedra) {
+    # creo un ctr_quarter que tenga en cuenta cuando
+    # los clientes hace 3 menos meses que estan
+    cat( "\n","Inicia variables intrames cátedra")
+    dataset[, ctrx_quarter_normalizado := ctrx_quarter]
+    dataset[cliente_antiguedad == 1, ctrx_quarter_normalizado := ctrx_quarter * 5]
+    dataset[cliente_antiguedad == 2, ctrx_quarter_normalizado := ctrx_quarter * 2]
+    dataset[cliente_antiguedad == 3, ctrx_quarter_normalizado := ctrx_quarter * 1.2]
   
-
-  # a partir de aqui juego con la suma de Mastercard y Visa
+    # variable extraida de una tesis de maestria de Irlanda
+    dataset[, mpayroll_sobre_edad := mpayroll / cliente_edad]
   
-  dataset[, vmr_Master_mlimitecompra := Master_mlimitecompra / vm_mlimitecompra]
-  dataset[, vmr_Visa_mlimitecompra := Visa_mlimitecompra / vm_mlimitecompra]
-  dataset[, vmr_msaldototal := vm_msaldototal / vm_mlimitecompra]
-  dataset[, vmr_msaldopesos := vm_msaldopesos / vm_mlimitecompra]
-  dataset[, vmr_msaldopesos2 := vm_msaldopesos / vm_msaldototal]
-  dataset[, vmr_msaldodolares := vm_msaldodolares / vm_mlimitecompra]
-  dataset[, vmr_msaldodolares2 := vm_msaldodolares / vm_msaldototal]
-  dataset[, vmr_mconsumospesos := vm_mconsumospesos / vm_mlimitecompra]
-  dataset[, vmr_mconsumosdolares := vm_mconsumosdolares / vm_mlimitecompra]
-  dataset[, vmr_madelantopesos := vm_madelantopesos / vm_mlimitecompra]
-  dataset[, vmr_madelantodolares := vm_madelantodolares / vm_mlimitecompra]
-  dataset[, vmr_mpagado := vm_mpagado / vm_mlimitecompra]
-  dataset[, vmr_mpagospesos := vm_mpagospesos / vm_mlimitecompra]
-  dataset[, vmr_mpagosdolares := vm_mpagosdolares / vm_mlimitecompra]
-  dataset[, vmr_mconsumototal := vm_mconsumototal / vm_mlimitecompra]
-  dataset[, vmr_mpagominimo := vm_mpagominimo / vm_mlimitecompra]
-
-  cat( "\n","Fin variables intrames cátedra")
-  # Aqui debe usted agregar sus propias nuevas variables
+    # se crean los nuevos campos para MasterCard  y Visa,
+    #  teniendo en cuenta los NA's
+    # varias formas de combinar Visa_status y Master_status
+    dataset[, vm_status01 := pmax(Master_status, Visa_status, na.rm = TRUE)]
+    dataset[, vm_status02 := Master_status + Visa_status]
+  
+    dataset[, vm_status03 := pmax(
+      ifelse(is.na(Master_status), 10, Master_status),
+      ifelse(is.na(Visa_status), 10, Visa_status)
+    )]
+  
+    dataset[, vm_status04 := ifelse(is.na(Master_status), 10, Master_status)
+      + ifelse(is.na(Visa_status), 10, Visa_status)]
+  
+    dataset[, vm_status05 := ifelse(is.na(Master_status), 10, Master_status)
+      + 100 * ifelse(is.na(Visa_status), 10, Visa_status)]
+  
+    dataset[, vm_status06 := ifelse(is.na(Visa_status),
+      ifelse(is.na(Master_status), 10, Master_status),
+      Visa_status
+    )]
+  
+    dataset[, mv_status07 := ifelse(is.na(Master_status),
+      ifelse(is.na(Visa_status), 10, Visa_status),
+      Master_status
+    )]
+  
+  
+    # combino MasterCard y Visa
+    dataset[, vm_mfinanciacion_limite := rowSums(cbind(Master_mfinanciacion_limite, Visa_mfinanciacion_limite), na.rm = TRUE)]
+  
+    dataset[, vm_Fvencimiento := pmin(Master_Fvencimiento, Visa_Fvencimiento, na.rm = TRUE)]
+    dataset[, vm_Finiciomora := pmin(Master_Finiciomora, Visa_Finiciomora, na.rm = TRUE)]
+    dataset[, vm_msaldototal := rowSums(cbind(Master_msaldototal, Visa_msaldototal), na.rm = TRUE)]
+    dataset[, vm_msaldopesos := rowSums(cbind(Master_msaldopesos, Visa_msaldopesos), na.rm = TRUE)]
+    dataset[, vm_msaldodolares := rowSums(cbind(Master_msaldodolares, Visa_msaldodolares), na.rm = TRUE)]
+    dataset[, vm_mconsumospesos := rowSums(cbind(Master_mconsumospesos, Visa_mconsumospesos), na.rm = TRUE)]
+    dataset[, vm_mconsumosdolares := rowSums(cbind(Master_mconsumosdolares, Visa_mconsumosdolares), na.rm = TRUE)]
+    dataset[, vm_mlimitecompra := rowSums(cbind(Master_mlimitecompra, Visa_mlimitecompra), na.rm = TRUE)]
+    dataset[, vm_madelantopesos := rowSums(cbind(Master_madelantopesos, Visa_madelantopesos), na.rm = TRUE)]
+    dataset[, vm_madelantodolares := rowSums(cbind(Master_madelantodolares, Visa_madelantodolares), na.rm = TRUE)]
+    dataset[, vm_fultimo_cierre := pmax(Master_fultimo_cierre, Visa_fultimo_cierre, na.rm = TRUE)]
+    dataset[, vm_mpagado := rowSums(cbind(Master_mpagado, Visa_mpagado), na.rm = TRUE)]
+    dataset[, vm_mpagospesos := rowSums(cbind(Master_mpagospesos, Visa_mpagospesos), na.rm = TRUE)]
+    dataset[, vm_mpagosdolares := rowSums(cbind(Master_mpagosdolares, Visa_mpagosdolares), na.rm = TRUE)]
+    dataset[, vm_fechaalta := pmax(Master_fechaalta, Visa_fechaalta, na.rm = TRUE)]
+    dataset[, vm_mconsumototal := rowSums(cbind(Master_mconsumototal, Visa_mconsumototal), na.rm = TRUE)]
+    dataset[, vm_cconsumos := rowSums(cbind(Master_cconsumos, Visa_cconsumos), na.rm = TRUE)]
+    dataset[, vm_cadelantosefectivo := rowSums(cbind(Master_cadelantosefectivo, Visa_cadelantosefectivo), na.rm = TRUE)]
+    dataset[, vm_mpagominimo := rowSums(cbind(Master_mpagominimo, Visa_mpagominimo), na.rm = TRUE)]
+    
+  
+    # a partir de aqui juego con la suma de Mastercard y Visa
+    
+    dataset[, vmr_Master_mlimitecompra := Master_mlimitecompra / vm_mlimitecompra]
+    dataset[, vmr_Visa_mlimitecompra := Visa_mlimitecompra / vm_mlimitecompra]
+    dataset[, vmr_msaldototal := vm_msaldototal / vm_mlimitecompra]
+    dataset[, vmr_msaldopesos := vm_msaldopesos / vm_mlimitecompra]
+    dataset[, vmr_msaldopesos2 := vm_msaldopesos / vm_msaldototal]
+    dataset[, vmr_msaldodolares := vm_msaldodolares / vm_mlimitecompra]
+    dataset[, vmr_msaldodolares2 := vm_msaldodolares / vm_msaldototal]
+    dataset[, vmr_mconsumospesos := vm_mconsumospesos / vm_mlimitecompra]
+    dataset[, vmr_mconsumosdolares := vm_mconsumosdolares / vm_mlimitecompra]
+    dataset[, vmr_madelantopesos := vm_madelantopesos / vm_mlimitecompra]
+    dataset[, vmr_madelantodolares := vm_madelantodolares / vm_mlimitecompra]
+    dataset[, vmr_mpagado := vm_mpagado / vm_mlimitecompra]
+    dataset[, vmr_mpagospesos := vm_mpagospesos / vm_mlimitecompra]
+    dataset[, vmr_mpagosdolares := vm_mpagosdolares / vm_mlimitecompra]
+    dataset[, vmr_mconsumototal := vm_mconsumototal / vm_mlimitecompra]
+    dataset[, vmr_mpagominimo := vm_mpagominimo / vm_mlimitecompra]
+  
+    cat( "\n","Fin variables intrames cátedra")
+  }
+    
+    # Aqui debe usted agregar sus propias nuevas variables
   if (PARAM$variables_iter1) {
     cat( "\n","Inicio variables combinadas agregadas iteración 1")
     dataset[, totconsumostcpesos := rowSums(cbind(mtarjeta_visa_consumo, mtarjeta_master_consumo), na.rm = TRUE)]
