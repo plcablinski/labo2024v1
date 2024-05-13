16dfd09f200da697a7bdf9a56239e15435b4d506
plcablinski <plcablinski@mail.austral.edu.ar>
2024-05-13 09:59:02 -0300

    Uso mejor bayesiana de 1006 hasta este momento.

---
+++ src/workflow-01/pc501_workflow_semillerio_13.r
@@ -0,0 +1,136 @@
+# Corrida general del Workflow de semillerio
+
+# limpio la memoria
+rm(list = ls(all.names = TRUE)) # remove all objects
+gc(full = TRUE) # garbage collection
+
+require("rlang")
+require("yaml")
+require("data.table")
+require("ParamHelpers")
+
+# creo environment global
+envg <- env()
+
+envg$EXPENV <- list()
+envg$EXPENV$exp_dir <- "~/buckets/b1/exp/"
+envg$EXPENV$wf_dir <- "~/buckets/b1/flow/"
+envg$EXPENV$wf_dir_local <- "~/flow/"
+envg$EXPENV$repo_dir <- "~/labo2024v1/"
+envg$EXPENV$datasets_dir <- "~/buckets/b1/datasets/"
+envg$EXPENV$arch_sem <- "mis_semillas.txt"
+
+# default
+envg$EXPENV$gcloud$RAM <- 64
+envg$EXPENV$gcloud$cCPU <- 8
+
+#------------------------------------------------------------------------------
+# Error catching
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  
+  cat(format(Sys.time(), "%Y%m%d %H%M%S"), "\n",
+    file = "z-Rabort.txt",
+    append = TRUE 
+    )
+
+  stop("exiting after script error")
+})
+#------------------------------------------------------------------------------
+# inicializaciones varias
+
+dir.create( envg$EXPENV$wf_dir, showWarnings = FALSE)
+dir.create( envg$EXPENV$wf_dir, showWarnings = FALSE)
+dir.create( envg$EXPENV$wf_dir_local, showWarnings = FALSE)
+setwd( envg$EXPENV$wf_dir_local )
+
+#------------------------------------------------------------------------------
+# cargo la  "libreria" de los experimentos
+
+exp_lib <- paste0( envg$EXPENV$repo_dir,"/src/lib/z590_exp_lib_01.r")
+source( exp_lib )
+
+#------------------------------------------------------------------------------
+# proceso ZZ_final  baseline
+
+ZZ_final_baseline <- function( pmyexp, pinputexps, pserver="local")
+{
+  if( -1 == (param_local <- exp_init( pmyexp, pinputexps, pserver ))$resultado ) return( 0 )# linea fija
+
+  param_local$meta$script <- "/src/workflow-01/z571_ZZ_final.r"
+
+  # Que modelos quiero, segun su posicion en el ranking e la Bayesian Optimizacion, ordenado por ganancia descendente
+  param_local$modelos_rank <- c(1)
+
+  param_local$kaggle$envios_desde <-  9500L
+  param_local$kaggle$envios_hasta <- 11500L
+  param_local$kaggle$envios_salto <-   500L
+
+  # para el caso que deba graficar
+  param_local$graficar$envios_desde <-  8000L
+  param_local$graficar$envios_hasta <- 20000L
+  param_local$graficar$ventana_suavizado <- 2001L
+
+  # Una corrida de Guantes Blancos solo usa 5 semillas
+  param_local$qsemillas <- 5
+
+  return( exp_correr_script( param_local ) ) # linea fija
+}
+#------------------------------------------------------------------------------
+# proceso ZZ_final  baseline
+
+ZZ_final_semillerio_baseline <- function( pmyexp, pinputexps, pserver="local")
+{
+  if( -1 == (param_local <- exp_init( pmyexp, pinputexps, pserver ))$resultado ) return( 0 )# linea fija
+
+  param_local$meta$script <- "/src/workflow-01/z881_ZZ_final_semillerio.r"
+
+  # Que modelos quiero, segun su posicion en el ranking e la Bayesian Optimizacion, ordenado por ganancia descendente
+  param_local$modelos_rank <- c(1)
+
+  param_local$kaggle$envios_desde <-  9500L
+  param_local$kaggle$envios_hasta <- 13000L
+  param_local$kaggle$envios_salto <-   500L
+
+  # para el caso que deba graficar
+  param_local$graficar$envios_desde <-  8000L
+  param_local$graficar$envios_hasta <- 20000L
+  param_local$graficar$ventana_suavizado <- 2001L
+
+  # El parametro fundamental de semillerio
+  # Es la cantidad de LightGBM's que ensamblo
+  param_local$semillerio <- 20
+
+  return( exp_correr_script( param_local ) ) # linea fija
+}
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+# A partir de ahora comienza la seccion de Workflows Completos
+#------------------------------------------------------------------------------
+# Este es el  Workflow baseline con semillerio
+# Que predice 202109
+# y ya genera archivos para Kaggle
+
+corrida_baseline_semillerio_202109 <- function( pnombrewf,pcorrida, pvirgen=FALSE )
+{
+  if( -1 == exp_wf_init( pnombrewf, pvirgen) ) return(0) # linea fija
+  # El ZZ depente de HT y TS
+  ZZ_final_semillerio_baseline( paste0("ZZ",pcorrida), c(paste0("HT",pcorrida),"TS1006"))
+  exp_wf_end( pnombrewf, pvirgen ) # linea fija
+}
+
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#Aqui empieza el programa
+
+
+corrida_baseline_semillerio_202109( "final13","1013" )
+
+
+
+
+ 
